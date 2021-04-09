e22190a5ba8a7182f8cfca45248d46653c47cd7d
IrfanCSE <irfanulhasan1996@gmail.com>
2021-04-09 19:01:10 +0600

    form setup

--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { ConfirmedValidator } from '../../core/confirmed.validator';
 
 @Component({
   selector: 'app-register',
@@ -7,10 +14,15 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  passMatch = true;
+
   registerForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+@[0-9]+$')]],
-    confirmPassword: ['', [Validators.required]],
+    password: [
+      '',
+      [Validators.required, Validators.pattern('^[a-zA-Z]+@[0-9]+$')],
+    ],
+    confirmPassword: ['', [Validators.required, ConfirmedValidator]],
     userName: ['', [Validators.required]],
     firstName: ['', [Validators.required]],
     lastName: ['', [Validators.required]],
@@ -20,11 +32,22 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {}
 
-  public get getRegister() {
+  public get getRegister(): any {
     return this.registerForm.controls;
   }
 
+  change = () => {
+    if (
+      this.registerForm.controls.password.value ===
+      this.registerForm.controls.confirmPassword.value
+    ) {
+      this.passMatch = true;
+    } else {
+      this.passMatch = false;
+    }
+  };
+
   onSubmit = () => {
-    console.log(this.registerForm.value);
+    console.log(this.getRegister.password.value);
   };
 }

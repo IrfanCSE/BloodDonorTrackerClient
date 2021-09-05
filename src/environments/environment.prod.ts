accccfdbee303e4f4571c583e84f6c5343e069ae
Irfanul Hasan <irfanulhasan1996@gmail.com>
2021-09-06 00:05:02 +0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://blood-donor.azurewebsites.net/api/'
+//   baseUrl: 'https://blood-donor.azurewebsites.net/api/',
+  baseUrl: 'https://bdt.azurewebsites.net/api/'
 };
